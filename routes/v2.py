210be9ea1dac681973302b69c6fe1d44c35943b3
JuanCarvajal12 <jcarvajal000@gmail.com>
2022-01-14 21:25:10 -0500

    POST /v2/user Auth update

--- routes/v2.py
+++ routes/v2.py
@@ -8,7 +8,7 @@ version 2. Further learning about FastAPI.
 """
 
 #%---
-from fastapi.param_functions import Header
+from fastapi.param_functions import Header, Depends
 from models.user import User
 from models.product import Product
 from models.supplier import Supplier
@@ -16,13 +16,18 @@ from fastapi import FastAPI, Header, File, Body
 from starlette.status import HTTP_201_CREATED # Import necessary status codes.
 from starlette.responses import Response
 
+from utils.security import check_jwt_token
+
 #%---
-app_v2 = FastAPI(openapi_prefix='/v2')
+app_v2 = FastAPI(root_path='/v2')
 
 #%---
 # POST /user
 # IMPORTANT: In python i wrote 'x_custom', but in the request I use 'x-custom'.
+## ADDED FUNCTIONALITY: Now this function requires authentication and authori-
+## zation. In the header of the request, should appear the Authorization entry
+## with value "Bearer <token>". The token is generated with POST -> /v1/token.
 @app_v2.post("/user", status_code=HTTP_201_CREATED) # endpoint
-async def post_user(user: User, x_custom: str = Header("default value")):
+async def post_user(user: User, x_custom: str = Header("default value"), jwt: bool = Depends(check_jwt_token)):
     return {"request body": {'user': user, "version": 'v2'},
             "request custom header": x_custom}
